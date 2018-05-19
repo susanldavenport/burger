b4bc912bf5ee87698ec3320bc7faa7d1755c4e54
SusanLDavenport <susanldavenport@gmail.com>
2018-05-18 22:29:39 -0400

    updated css, handlebars, database not connecting

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@
 
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Slider', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
